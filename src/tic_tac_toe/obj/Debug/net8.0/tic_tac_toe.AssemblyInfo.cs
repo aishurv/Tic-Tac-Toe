0f8894eead90d0b8dfe39eae80b2706ed7972f17
aishurv <Laxmi.wandare@klingelenberg.com>
2024-12-18 17:08:15 +0530

    Add isWin method

--- src/tic_tac_toe/obj/Debug/net8.0/tic_tac_toe.AssemblyInfo.cs
+++ src/tic_tac_toe/obj/Debug/net8.0/tic_tac_toe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tic_tac_toe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a510622e613f26c8339ab4bde2e9c91c212b525a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c91886caa812f0d192e085a917192fad2ad05d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("tic_tac_toe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tic_tac_toe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
